78ecc7a22915645a1e2e106cd9ad9ad89acd3aae
n.topal <n.topal@ebebek.com>
2025-02-10 10:40:48 +0300

    New job project

---
+++ src/main/java/com/betus/jobexpnew/JobExpNewApplication.java
@@ -0,0 +1,13 @@
+package com.betus.jobexpnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobExpNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobExpNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/betus/jobexpnew/JobExpNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betus.jobexpnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobExpNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
